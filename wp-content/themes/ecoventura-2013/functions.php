36a66d10415456e437611815117ba6ba77a218ea
Nate Finch <finchproservices@gmail.com>
2017-06-14 07:37:15 +0200

    add current Genesis sample scss files, combine new functions.php with old functions.php, ready to begin

--- wp-content/themes/ecoventura-2013/functions.php
+++ wp-content/themes/ecoventura-2013/functions.php
@@ -28,6 +28,24 @@ function child_theme_setup() {
 	// Start the engine
 	require_once( get_template_directory() . '/lib/init.php' );
 
+	// Add the helper functions.
+	include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
+
+	// Add Image upload and Color select to WordPress Theme Customizer.
+	require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+	// Include Customizer CSS.
+	include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+	// Add WooCommerce support.
+	include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+	// Add the required WooCommerce styles and Customizer CSS.
+	include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+	// Add the Genesis Connect WooCommerce notice.
+	include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
 	// Child theme (do not remove)
 	define( 'CHILD_THEME_NAME', 'Ecoventura 2013' );
 	define( 'CHILD_THEME_URL', 'http://www.josheaton.org/' );
@@ -55,13 +73,50 @@ function child_theme_setup() {
 	// Add menus
 	add_action( 'init', 'eco_register_menus' );
 	// Move secondary menu to header
-	remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-	add_action( 'eco_top_bar', 'genesis_do_subnav' );
+	// remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+	// add_action( 'eco_top_bar', 'genesis_do_subnav' );
+
+	// Reposition the secondary navigation menu.
+	// remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+	// add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+	// Reduce the secondary navigation menu to one level depth.
+	add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+	function genesis_sample_secondary_menu_args( $args ) {
+
+		if ( 'secondary' != $args['theme_location'] ) {
+			return $args;
+		}
+
+		$args['depth'] = 1;
+
+		return $args;
+
+	}
+
+	// Modify size of the Gravatar in the author box.
+	add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+	function genesis_sample_author_box_gravatar( $size ) {
+		return 90;
+	}
+
+	// Modify size of the Gravatar in the entry comments.
+	add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+	function genesis_sample_comments_gravatar( $args ) {
+
+		$args['avatar_size'] = 60;
+
+		return $args;
+
+	}
 
 	/** Unregister layout settings */
 	genesis_unregister_layout( 'content-sidebar-sidebar' );
 	genesis_unregister_layout( 'sidebar-content-sidebar' );
 	genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+	//* Force full-width-content layout setting
+	add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 	// Image sizes
 	add_image_size( 'media-thumb',            52             );
@@ -82,13 +137,28 @@ function child_theme_setup() {
 	// * Frontend *
 
 	//* Add HTML5 markup structure
-	add_theme_support( 'genesis-html5' ); // beta
-	add_theme_support( 'html5' ); // 2.0
+	// add_theme_support( 'genesis-html5' ); // beta
+	// add_theme_support( 'html5' ); // 2.0
+
+	// Add HTML5 markup structure.
+	add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
 
 	add_action( 'wp_head', 'eco_favicons', 1 );
 
+	// Add Accessibility support.
+	add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
 	//* Add viewport meta tag for mobile browsers
 	add_theme_support( 'genesis-responsive-viewport' );
+
+	// Add support for custom header.
+	add_theme_support( 'custom-header', array(
+		'width'           => 600,
+		'height'          => 160,
+		'header-selector' => '.site-title a',
+		'header-text'     => false,
+		'flex-height'     => true,
+	) );
 
 	// Enqueue scripts and styles
 	add_action( 'wp_enqueue_scripts', 'eco_scripts' );
@@ -103,7 +173,16 @@ function child_theme_setup() {
 	add_action( 'genesis_before_header', 'eco_top_bar', 3 );
 
 	// Add logo to the header
-	add_filter( 'genesis_seo_title', 'eco_custom_header_logo', 10, 3 );
+	// add_filter( 'genesis_seo_title', 'eco_custom_header_logo', 10, 3 );
+
+	// Add support for custom background.
+	add_theme_support( 'custom-background' );
+
+	// Add support for after entry widget.
+	add_theme_support( 'genesis-after-entry-widget-area' );
+
+	// Rename primary and secondary navigation menus.
+	// add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 
 	// Remove Genesis front-end features
 	// * Header *
@@ -154,6 +233,37 @@ function eco_scripts() {
 	wp_enqueue_script( 'colorbox', get_stylesheet_directory_uri() . '/lib/js/colorbox/jquery.colorbox.min.js', array('jquery'), filemtime(get_stylesheet_directory() . '/lib/js/colorbox/jquery.colorbox.min.js'), true );
 	wp_enqueue_script( 'resp-tables-js', get_stylesheet_directory_uri() . '/lib/js/responsive-tables/responsive-tables.js', array('jquery'), filemtime(get_stylesheet_directory() . '/lib/js/responsive-tables/responsive-tables.js'), true );
 	wp_enqueue_script( 'eco-global', get_stylesheet_directory_uri() . '/js/global.min.js', array('jquery','colorbox'), filemtime(get_stylesheet_directory() . '/js/global.min.js'), true );
+
+
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+}
+
+// Define our responsive menu settings.
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
 }
 
 function eco_google_fonts() {
