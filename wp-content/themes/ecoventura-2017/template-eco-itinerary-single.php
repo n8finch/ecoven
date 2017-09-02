a84706ce90f742c221b2f7e85614f396aa8b7958
Nate Finch <finchproservices@gmail.com>
2017-09-01 19:05:39 -0500

    single itinerary page added, other pages slighted updated

--- wp-content/themes/ecoventura-2017/template-eco-itinerary-single.php
+++ wp-content/themes/ecoventura-2017/template-eco-itinerary-single.php
@@ -1,6 +1,6 @@
 <?php
 
-//* Template Name: Ecoventura Itinerary A/B
+//* Template Name: Ecoventura Itinerary Single
 //* Force full width content layout
 add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 
@@ -16,12 +16,13 @@ function eco_rates_content() {
 	$acf_fields = get_fields( $post->ID );
 
 	//* Add in the sections
-	eco_intinerary_a( $acf_fields);
+	eco_intinerary_single( $acf_fields);
 	eco_intinerary_b( $acf_fields);
+	eco_intinerary_single_popup( $acf_fields );
 
 }
 
-function eco_intinerary_a( $acf_fields ) {
+function eco_intinerary_single( $acf_fields ) {
 	?>
 	<header class="itinerary-header">
 		<h2><?php echo esc_html( get_the_title() ); ?></h2>
@@ -73,62 +74,6 @@ function eco_intinerary_a( $acf_fields ) {
 			<div class="book-now-box">
 				<a href="<?php echo esc_url( $acf_fields[ 'itinerary_a_link' ] );?>">
 					<button><?php echo esc_html( $acf_fields[ 'itinerary_a_button_text' ] );?></button>
-				</a>
-			</div>
-		</div>
-	</section>
-	<?php
-}
-
-function eco_intinerary_b( $acf_fields ) {
-	?>
-	<section class="itinerary-section">
-		<img src="<?php echo esc_attr( $acf_fields['itinerary_b_image'] ); ?>" />
-		<div class="view-iteneraries">
-			<a href="#">
-				<div id="view-itens">VIEW ITENERARIES</div>
-				<div id="arrow-right"></div>
-			</a>
-			<a href="<?php echo esc_url( $acf_fields[ 'itinerary_b_link' ] );?>">
-				<div id="arrow-from-right-b"></div>
-				<div id="iten-b">ITENERARY B</div>
-			</a>
-		</div>
-		<div class="itinerary-content">
-			<?php echo wp_kses_post( $acf_fields['itinerary_b_content'] ); ?>
-		</div>
-		<div class="itinerary-highlights">
-			<h2>HIGHLIGHTS</h2>
-			<div class="highlight-boxes">
-				<?php
-				$rows = $acf_fields['itinerary_b_highlights'];
-				if($rows) {
-					$counter = 1;
-					foreach ( $rows as $row ) {
-						$pop_id = 'iten-b-popup-' . $counter;
-					?>
-						<div class="highlight-box">
-							<div class="image-container" data-popup-id="<?php echo esc_html( $pop_id ); ?>">
-								<img src="<?php echo esc_attr( $row['image'] ); ?>" data-popup-id="<?php echo esc_html( $pop_id ); ?>"/>
-							</div>
-							<div class="hr-blue"></div>
-							<p class="image-subtitle"><?php echo wp_kses_post( $row['image_subtitle'] ); ?></p>
-
-							<div id="<?php echo esc_html( $pop_id ); ?>" visibility="hidden" class="iteneraries-popup">
-								<img src="<?php echo esc_attr( $row['popup_image'] );?>" />
-								<p><?php echo wp_kses_post( $row['popup_facts'] ); ?></p>
-							</div>
-						</div>
-					<?php
-					$counter++;
-					} //end foreach
-				} //end if ?>
-
-
-			</div>
-			<div class="book-now-box">
-				<a href="<?php echo esc_url( $acf_fields[ 'itinerary_b_link' ] );?>">
-					<button><?php echo esc_html( $acf_fields[ 'itinerary_b_button_text' ] );?></button>
 				</a>
 			</div>
 		</div>
