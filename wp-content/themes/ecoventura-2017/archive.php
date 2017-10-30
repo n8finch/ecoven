4b924cbdd2e4debba5a55f1a93e36a689488d66b
Josh Eaton <josh@josheaton.org>
2017-10-29 20:10:59 -0400

    Fix guest review styling

--- wp-content/themes/ecoventura-2017/archive.php
+++ wp-content/themes/ecoventura-2017/archive.php
@@ -5,16 +5,4 @@ add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 // Remove the default Genesis loop
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
-
-// Add custom rates content
-add_action( 'genesis_before_loop', 'eco_rates_content_header' );
-
-function eco_rates_content_header() {
-	?>
-	<header class="content-header">
-		<h2><?php echo esc_html( get_the_title() ); ?></h2>
-	</header>
-	<?php
-}
-
 genesis();
