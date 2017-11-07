a7f1ab1badd63d7d9ecdfc16d099568cbdc554ad
Josh Eaton <josh@josheaton.org>
2017-11-06 21:14:35 -0500

    Add back get_page_by_slug

--- wp-content/themes/ecoventura-2017/inc/eco-functions.php
+++ wp-content/themes/ecoventura-2017/inc/eco-functions.php
@@ -156,3 +156,22 @@ function eco_gallery_slideshow( $val, $attr ) {
 
 	return $output;
 }
+
+/**
+ * Utility function to get a post object from a page slug
+ *
+ * @param  [type] $page_slug [description]
+ * @param  [type] $output    [description]
+ * @param  string $post_type [description]
+ * @return [type]            [description]
+ */
+function get_page_by_slug( $page_slug, $output = OBJECT, $post_type = 'page' ) {
+	global $wpdb;
+
+	$page = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_type= %s AND post_status = 'publish'", $page_slug, $post_type ) );
+
+	 if ( $page )
+		return get_post($page, $output);
+
+	return null;
+}
